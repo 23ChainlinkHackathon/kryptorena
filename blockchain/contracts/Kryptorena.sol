efd708c8e8ad253028d846aa34de7d25cb0b71fa
robitu <robertoiturralde2009@hotmail.com>
2023-05-23 17:49:46 -0600

    connected battle contract with game logic

--- blockchain/contracts/Kryptorena.sol
+++ blockchain/contracts/Kryptorena.sol
@@ -5,6 +5,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "./KryptorenaNft.sol";
+import "./KryptorenaBattle.sol";
 
 contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
     enum GameStatus {
@@ -26,7 +27,7 @@ contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
         bool inGame; // true if playing if by any chance player left the game, we need to put it false
     }
 
-    //storing the battle data
+    //     //storing the battle data
 
     struct Game {
         GameStatus gameStatus;
@@ -45,6 +46,7 @@ contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
         uint defence;
     }
 
+    KryptorenaBattle public i_kryptorenaBattle;
     KryptorenaNft public i_kryptorenaNft;
     uint256 public i_mintFee;
     uint256 s_randomAttackValue;
@@ -68,11 +70,11 @@ contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
-    // events
+    //     // events
 
-    event newPlayer(address indexed owner, string name);
-    event newGame(string battleName, address indexed player1, address indexed player2);
-    event gameEnded(string battleName, address indexed winner, address indexed defeated);
+    //     event newPlayer(address indexed owner, string name);
+    //     event newGame(string battleName, address indexed player1, address indexed player2);
+    //     event gameEnded(string battleName, address indexed winner, address indexed defeated);
 
     constructor(
         address vrfCoordinatorV2,
@@ -80,119 +82,131 @@ contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
         bytes32 gasLane,
         uint32 callbackGasLimit,
         string[] memory cardTokenUris,
-        address kryptorenaNft,
-        uint256 mintFee
+        address kryptorenaNftAddress,
+        uint256 mintFee,
+        address kryptorenaBattleAddress
     ) VRFConsumerBaseV2(vrfCoordinatorV2) ConfirmedOwner(msg.sender) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
         i_callbackGasLimit = callbackGasLimit;
-        i_kryptorenaNft = KryptorenaNft(kryptorenaNft);
+        i_kryptorenaNft = KryptorenaNft(kryptorenaNftAddress);
         i_mintFee = mintFee;
+        i_kryptorenaBattle = KryptorenaBattle(kryptorenaBattleAddress);
     }
 
-    // register the player
+    //     // register the player
 
-    function registerNewPlayer(string memory _name) external returns (uint256 requestId) {
-        require(!isPlayer(msg.sender), "This address already registered");
-        require(msg.value >= i_mintFee, "Not enough AVAX");
+    //     function registerNewPlayer(string memory _name) external payable returns (uint256 requestId) {
+    //         require(!isPlayer(msg.sender), "This address already registered");
+    //         require(msg.value >= i_mintFee, "Not enough AVAX");
 
-        i_kryptorenaNft.requestNft{value: msg.value}();
+    //         i_kryptorenaNft.requestNft{value: msg.value}();
 
-        uint _id = players.length;
-        players.push(Player(_name, msg.sender, false));
-        playerInfo[msg.sender] = _id;
+    //         uint _id = players.length;
+    //         players.push(Player(_name, msg.sender, false));
+    //         playerInfo[msg.sender] = _id;
 
-        requestId = i_vrfCoordinator.requestRandomWords(
-            i_gasLane,
-            i_subscriptionId,
-            REQUEST_CONFIRMATIONS,
-            i_callbackGasLimit,
-            NUM_WORDS
-        );
-        s_requestIdToSender[requestId] = msg.sender;
-        s_addressToUsername[msg.sender] = _name;
+    //         requestId = i_vrfCoordinator.requestRandomWords(
+    //             i_gasLane,
+    //             i_subscriptionId,
+    //             REQUEST_CONFIRMATIONS,
+    //             i_callbackGasLimit,
+    //             NUM_WORDS
+    //         );
+    //         s_requestIdToSender[requestId] = msg.sender;
+    //         s_addressToUsername[msg.sender] = _name;
 
-        emit newPlayer(msg.sender, _name);
+    //         emit newPlayer(msg.sender, _name);
+    //     }
+
+    //     /**
+    //      * @notice Battle contract will call this function to send winner's updated stats
+    //      * @notice This function will then update the player struct with the new stats.
+    //      */
+
+    function updateStats(address winner, uint256 newAttack, uint256 newDefense) external {
+        require(msg.sender == address(i_kryptorenaBattle), "Unauthorized");
+        // logic
     }
 
-    // create Battle
+    //     // create Battle
 
-    function createGame(string memory _name) external returns (Game memory) {
-        require(isPlayer(msg.sender), "This address is not registered"); // Require that the player is registered
-        require(!isGame(_name), "Can not create game with this name. It already exist."); // Require battle with same name should not exist
+    //     function createGame(string memory _name) external returns (Game memory) {
+    //         require(isPlayer(msg.sender), "This address is not registered"); // Require that the player is registered
+    //         require(!isGame(_name), "Can not create game with this name. It already exist."); // Require battle with same name should not exist
 
-        bytes32 hashOfBattle = keccak256(abi.encode(_name));
+    //         bytes32 hashOfBattle = keccak256(abi.encode(_name));
 
-        Game memory _game = Game(
-            GameStatus.PENDING,
-            _name,
-            hashOfBattle,
-            [msg.sender, address(0)],
-            [0, 0],
-            address(0)
-        );
+    //         Game memory _game = Game(
+    //             GameStatus.PENDING,
+    //             _name,
+    //             hashOfBattle,
+    //             [msg.sender, address(0)],
+    //             [0, 0],
+    //             address(0)
+    //         );
 
-        uint _id = games.length;
-        gameInfo[_name] = _id;
-        games.push(_game);
+    //         uint _id = games.length;
+    //         gameInfo[_name] = _id;
+    //         games.push(_game);
 
-        return _game;
-    }
+    //         return _game;
+    //     }
 
-    function joinGame(string memory _name) external returns (Game memory) {
-        Game memory _game = getGame(_name);
+    //     // function joinGame(string memory _name) external returns (Game memory) {
+    //     //     Game memory _game = getGame(_name);
 
-        require(_game.gameStatus == GameStatus.PENDING, "Battle already started!"); // Require that battle has not started
-        require(_game.playersInBattle[0] != msg.sender, "Only player two can join a battle"); // Require that player 2 is joining the battle
-        require(!getPlayer(msg.sender).inBattle, "Already in battle"); // Require that player is not already in a battle
+    //     //     require(_game.gameStatus == GameStatus.PENDING, "Battle already started!"); // Require that battle has not started
+    //     //     require(_game.playersInBattle[0] != msg.sender, "Only player two can join a battle"); // Require that player 2 is joining the battle
+    //     //     require(!getPlayer(msg.sender).inBattle, "Already in battle"); // Require that player is not already in a battle
 
-        _game.battleStatus = GameStatus.STARTED;
-        _game.players[1] = msg.sender;
-        updateGame(_name, _game);
+    //     //     _game.battleStatus = GameStatus.STARTED;
+    //     //     _game.players[1] = msg.sender;
+    //     //     updateGame(_name, _game);
 
-        players[playerInfo[_game.players[0]]].inBattle = true;
-        players[playerInfo[_game.players[1]]].inBattle = true;
+    //     //     players[playerInfo[_game.players[0]]].inBattle = true;
+    //     //     players[playerInfo[_game.players[1]]].inBattle = true;
 
-        emit newGame(_game.name, _game.players[0], msg.sender); // Emits NewBattle event
-        return _game;
-    }
+    //     //     emit newGame(_game.name, _game.players[0], msg.sender); // Emits NewBattle event
+    //     //     return _game;
+    //     // }
 
-    function getPlayer(address _address) public {}
+    //     function getPlayer(address _address) public {}
 
-    function getAllPlayer(address _address) public {}
+    //     function getAllPlayer(address _address) public {}
 
-    function getGame(address _address) public {}
+    //     function getGame(address _address) public {}
 
-    function getAllGame(address _address) public {}
+    //     function getAllGame(address _address) public {}
 
-    function updateGame() public {}
+    //     function updateGame() public {}
 
-    function generateRandomNumber() public {}
+    //     function generateRandomNumber() public {}
 
-    function generateRandomGameId() public {}
+    //     function generateRandomGameId() public {}
 
-    function joinGame() public {}
+    //     function joinGame() public {}
 
-    function aodBattle() public {
-        // to get if user chosed attack or defence
-    }
+    //     function aodBattle() public {
+    //         // to get if user chosed attack or defence
+    //     }
 
-    function regAod() public {
-        // to store the choice on-chain
-    }
+    //     function regAod() public {
+    //         // to store the choice on-chain
+    //     }
 
-    function quitGame(string memory _gameName) public {
-        Game memory _game = getGame(_gameName);
-        require(
-            _game.players[0] == msg.sender ||
-                _game.players[1] = -msg.sender,
-            "You are not in this game sir"
-        );
-        _game.players[0] == msg.sender
-            ? endGame(_game.players[1], _game)
-            : endGame(_game.players[0], _game);
-    }
+    //     // function quitGame(string memory _gameName) public {
+    //     //     Game memory _game = getGame(_gameName);
+    //     //     require(
+    //     //         _game.players[0] == msg.sender ||
+    //     //             _game.players[1] = -msg.sender,
+    //     //         "You are not in this game sir"
+    //     //     );
+    //     //     _game.players[0] == msg.sender
+    //     //         ? endGame(_game.players[1], _game)
+    //     //         : endGame(_game.players[0], _game);
+    //     // }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         uint256 randomNumber = randomWords[0];
@@ -210,45 +224,45 @@ contract Kryptorena is VRFConsumerBaseV2, ConfirmedOwner {
         );
     }
 
-    function endGame(address lastUser, Game memory _game) internal returns (Game memory) {
-        require(_game.GameStatus != GameStatus.ENDED, "The game has ended");
-        _game.gameStatus = GameStatus.ENDED;
-        _game.winner = lastUser;
-        updateGame(_game.name, _game);
+    //     // function endGame(address lastUser, Game memory _game) internal returns (Game memory) {
+    //     //     require(_game.GameStatus != GameStatus.ENDED, "The game has ended");
+    //     //     _game.gameStatus = GameStatus.ENDED;
+    //     //     _game.winner = lastUser;
+    //     //     updateGame(_game.name, _game);
 
-        uint player1 = playerInfo[_game.players[0]];
-        uint player2 = playerInfo[_game.players[1]];
+    //     //     uint player1 = playerInfo[_game.players[0]];
+    //     //     uint player2 = playerInfo[_game.players[1]];
 
-        players[player1].inGame = false;
-        players[player1].playerHealth = 10;
-        players[player2].inGame = false;
-        players[player2].playerHealth = 10;
+    //     //     players[player1].inGame = false;
+    //     //     players[player1].playerHealth = 10;
+    //     //     players[player2].inGame = false;
+    //     //     players[player2].playerHealth = 10;
 
-        address _gameLoser = lastUser == _game.players[0] ? _game.players[1] : _game.players[0];
+    //     //     address _gameLoser = lastUser == _game.players[0] ? _game.players[1] : _game.players[0];
 
-        emit gameEnded(_game.name, lastUser, _gameLoser); // Emits BattleEnded event
+    //     //     emit gameEnded(_game.name, lastUser, _gameLoser); // Emits BattleEnded event
 
-        return _game;
-    }
+    //     //     return _game;
+    //     // }
 
-    function isPlayer(address _address) public view returns (bool) {
-        if (playerInfo[_address] == 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+    //     function isPlayer(address _address) public view returns (bool) {
+    //         if (playerInfo[_address] == 0) {
+    //             return false;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
 
-    function getGame(string memory _name) public view returns (Game memory) {
-        require(isGame(_name), "Battle doesn't exist!");
-        return games[gameInfo[_name]];
-    }
+    //     function getGame(string memory _name) public view returns (Game memory) {
+    //         require(isGame(_name), "Battle doesn't exist!");
+    //         return games[gameInfo[_name]];
+    //     }
 
-    function isGame(string memory _name) public view returns (bool) {
-        if (gameInfo[_name] == 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+    //     function isGame(string memory _name) public view returns (bool) {
+    //         if (gameInfo[_name] == 0) {
+    //             return false;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
 }
