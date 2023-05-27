b6c4729720b7dcd2641f56095102eafc5daf1e6b
Balendu Rawat <balendurawat1@gmail.com>
2023-05-27 08:08:52 +0530

    frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,62 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Battleground, CreateBattle, Battle, Home, JoinBattle, LandingPage } from './page';
+import { Battleground, CreateBattle, Battle, Home, JoinBattle} from './page';
 import { OnboardModal } from './components';
 import { GlobalContextProvider } from './context';
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      {/* after user click on play button in the landing page */}
-      <Route
-        path="/game"
-        element={
-          <GlobalContextProvider>
-            <Home />
-            <OnboardModal />
-          </GlobalContextProvider>
-        }
-      />
-      <Route
-        path="/battleground"
-        element={
-          <GlobalContextProvider>
-            <Battleground />
-            <OnboardModal />
-          </GlobalContextProvider>
-        }
-      />
-      <Route
-        path="/battle/:battleName"
-        element={
-          <GlobalContextProvider>
-            <Battle />
-            <OnboardModal />
-          </GlobalContextProvider>
-        }
-      />
-      <Route
-        path="/create-battle"
-        element={
-          <GlobalContextProvider>
-            <CreateBattle />
-            <OnboardModal />
-          </GlobalContextProvider>
-        }
-      />
-      <Route
-        path="/join-battle"
-        element={
-          <GlobalContextProvider>
-            <JoinBattle />
-            <OnboardModal />
-          </GlobalContextProvider>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+    <GlobalContextProvider>
+      <OnboardModal />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battleground" element={<Battleground />} />
+        <Route path="/battle/:battleName" element={<Battle />} />
+        <Route path="/create-battle" element={<CreateBattle />} />
+        <Route path="/join-battle" element={<JoinBattle />} />
+      </Routes>
+    </GlobalContextProvider>
+  </BrowserRouter>,
 );
